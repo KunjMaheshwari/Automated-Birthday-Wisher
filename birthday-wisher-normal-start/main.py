7f8336d1abf6c87358fb3b43a35117a5522b6684
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2022-12-29 12:43:21 +0530

    Update main.py

--- birthday-wisher-normal-start/main.py
+++ birthday-wisher-normal-start/main.py
@@ -1,32 +1,29 @@
 import smtplib
-import datetime as dt
-import pandas
 import random
+import pandas
+import datetime as dt
 
 my_email = "kunj1766@gmail.com"
 password = "1234"
 
-# creating a tuple
-today = (dt.datetime.now().month, dt.datetime.now().day)
+today = dt.datetime.now()
 
-# reading the birthday csv
-data = pandas.read_csv("birthdays.csv")
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthday.csv")
 data.to_dict(orient="records")
 
-new_dict = {(data_row["month"], (data_row["day"]))
-             : data_row for (index, data_row) in data.iterrows()}
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
-if today in new_dict:
-    birthday_person = new_dict[today]
-    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
-    with open(file_path) as letter:
-        contents = letter.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+if today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today_tuple]
+    file_path = f"letter_tempelate/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents.remove("[NAME]", birthday_person["name"])
 
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
-        connection.login(user=my_email, password=password)
+        connection.login(my_email, password)
         connection.sendmail(
-            from_addr=my_email,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday\n\n{contents}")
+            from_addr=my_email, to_addrs=birthday_person["email"], msg=f"Subject:Greetings\n\n{contents}")
